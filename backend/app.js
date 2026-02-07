0f788a1eb6f29bb9eea26a45e017d0cc0dc25617
Molnár Dóra <yunothefurry@gmail.com>
2026-02-07 12:02:32 +0100

    app

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+import rentalRoutes from "./routes/rentalRoutes.js";
+import { errorMiddleware } from "./middleware/errorMiddleware.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/rentals", rentalRoutes);
+
+app.use(errorMiddleware);
+
+export default app;
