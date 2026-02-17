4bdeec5a15ee20aa00880210984ed323e615dfa5
kalifaszi01 <petbal586@logiker.hu>
2026-02-17 12:23:35 +0100

    gombok

--- asztali/BookStore/BookStore.Gui/Form1.Designer.cs
+++ asztali/BookStore/BookStore.Gui/Form1.Designer.cs
@@ -28,82 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.Dashboard_button = new System.Windows.Forms.Button();
+            this.Books_button = new System.Windows.Forms.Button();
+            this.User_button = new System.Windows.Forms.Button();
+            this.Rental_button = new System.Windows.Forms.Button();
+            this.Logout_button = new System.Windows.Forms.Button();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // Dashboard_button
             // 
-            this.button1.Location = new System.Drawing.Point(47, 45);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Dashboard";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Dashboard_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Dashboard_button.FlatAppearance.BorderSize = 0;
+            this.Dashboard_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Dashboard_button.ForeColor = System.Drawing.Color.White;
+            this.Dashboard_button.Location = new System.Drawing.Point(0, 200);
+            this.Dashboard_button.Name = "Dashboard_button";
+            this.Dashboard_button.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.Dashboard_button.Size = new System.Drawing.Size(200, 49);
+            this.Dashboard_button.TabIndex = 0;
+            this.Dashboard_button.Text = "Dashboard";
+            this.Dashboard_button.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // Books_button
             // 
-            this.button2.Location = new System.Drawing.Point(47, 97);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Könyvek";
-            this.button2.UseVisualStyleBackColor = true;
+            this.Books_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Books_button.FlatAppearance.BorderSize = 0;
+            this.Books_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Books_button.ForeColor = System.Drawing.Color.White;
+            this.Books_button.Location = new System.Drawing.Point(0, 100);
+            this.Books_button.Name = "Books_button";
+            this.Books_button.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.Books_button.Size = new System.Drawing.Size(200, 50);
+            this.Books_button.TabIndex = 1;
+            this.Books_button.Text = "Könyvek";
+            this.Books_button.UseVisualStyleBackColor = true;
+            this.Books_button.Click += new System.EventHandler(this.Books_button_Click);
             // 
-            // button3
+            // User_button
             // 
-            this.button3.Location = new System.Drawing.Point(47, 155);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Felhasználók";
-            this.button3.UseVisualStyleBackColor = true;
+            this.User_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.User_button.FlatAppearance.BorderSize = 0;
+            this.User_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.User_button.ForeColor = System.Drawing.Color.White;
+            this.User_button.Location = new System.Drawing.Point(0, 0);
+            this.User_button.Name = "User_button";
+            this.User_button.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.User_button.Size = new System.Drawing.Size(200, 50);
+            this.User_button.TabIndex = 2;
+            this.User_button.Text = "Felhasználók";
+            this.User_button.UseVisualStyleBackColor = true;
+            this.User_button.Click += new System.EventHandler(this.User_button_Click);
             // 
-            // button4
+            // Rental_button
             // 
-            this.button4.Location = new System.Drawing.Point(47, 224);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Kölcsönzések";
-            this.button4.UseVisualStyleBackColor = true;
+            this.Rental_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Rental_button.FlatAppearance.BorderSize = 0;
+            this.Rental_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Rental_button.ForeColor = System.Drawing.Color.White;
+            this.Rental_button.Location = new System.Drawing.Point(0, 50);
+            this.Rental_button.Name = "Rental_button";
+            this.Rental_button.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.Rental_button.Size = new System.Drawing.Size(200, 50);
+            this.Rental_button.TabIndex = 3;
+            this.Rental_button.Text = "Kölcsönzések";
+            this.Rental_button.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // Logout_button
             // 
-            this.button5.Location = new System.Drawing.Point(47, 276);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Kijelentkezés";
-            this.button5.UseVisualStyleBackColor = true;
+            this.Logout_button.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Logout_button.FlatAppearance.BorderSize = 0;
+            this.Logout_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Logout_button.ForeColor = System.Drawing.Color.White;
+            this.Logout_button.Location = new System.Drawing.Point(0, 150);
+            this.Logout_button.Name = "Logout_button";
+            this.Logout_button.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.Logout_button.Size = new System.Drawing.Size(200, 50);
+            this.Logout_button.TabIndex = 4;
+            this.Logout_button.Text = "Kijelentkezés";
+            this.Logout_button.UseVisualStyleBackColor = true;
+            this.Logout_button.Click += new System.EventHandler(this.Logout_button_Click);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.panelMenu.Controls.Add(this.Dashboard_button);
+            this.panelMenu.Controls.Add(this.Logout_button);
+            this.panelMenu.Controls.Add(this.Books_button);
+            this.panelMenu.Controls.Add(this.Rental_button);
+            this.panelMenu.Controls.Add(this.User_button);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 450);
+            this.panelMenu.TabIndex = 5;
+            // 
+            // panelMain
+            // 
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(200, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(600, 450);
+            this.panelMain.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panelMenu);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button Dashboard_button;
+        private System.Windows.Forms.Button Books_button;
+        private System.Windows.Forms.Button User_button;
+        private System.Windows.Forms.Button Rental_button;
+        private System.Windows.Forms.Button Logout_button;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelMain;
     }
 }
 
--- asztali/BookStore/BookStore.Gui/Form1.cs
+++ asztali/BookStore/BookStore.Gui/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookStore.Gui.Forms;
+using BookStore.Gui.Helpers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +21,37 @@ namespace BookStore.Gui
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
+        private void LoadForm(Form form)
+        {
+            panelMain.Controls.Clear();
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+            panelMain.Controls.Add(form);
+            form.Show();
 
+        }
+
+        private void Books_button_Click(object sender, EventArgs e)
+        {
+
+            LoadForm(new BooksForm());
+        }
+
+      
+
+        private void User_button_Click(object sender, EventArgs e)
+        {
+            LoadForm(new UsersForm());
+        }
+
+        private void Logout_button_Click(object sender, EventArgs e)
+        {
+            TokenStorage.Token = null;
+            this.Hide();
+            new LoginForm().Show();
         }
     }
 }
