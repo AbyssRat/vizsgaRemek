a4dccc8e1b973bc1679f4c090143b5c6ba38d2d1
Bálint Petrény-Barócsy <petbal586@logiker.hu>
2026-03-04 19:00:49 +0100

    Book get post stb kész fél úton összedölt minden

--- asztali/BookStore/Bookstore/Form_Book.Designer.cs
+++ asztali/BookStore/Bookstore/Form_Book.Designer.cs
@@ -30,13 +30,25 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox_exit = new System.Windows.Forms.PictureBox();
+            this.listBoxBooks = new System.Windows.Forms.ListBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtGenre = new System.Windows.Forms.TextBox();
+            this.txtLanguage = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.txtFileUrl = new System.Windows.Forms.TextBox();
+            this.txtPreviewUrl = new System.Windows.Forms.TextBox();
+            this.txtCoverUrl = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_exit)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(366, 195);
+            this.label1.Location = new System.Drawing.Point(383, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 0;
@@ -53,16 +65,124 @@
             this.pictureBox_exit.TabStop = false;
             this.pictureBox_exit.Click += new System.EventHandler(this.pictureBox_exit_Click);
             // 
+            // listBoxBooks
+            // 
+            this.listBoxBooks.FormattingEnabled = true;
+            this.listBoxBooks.Location = new System.Drawing.Point(32, 48);
+            this.listBoxBooks.Name = "listBoxBooks";
+            this.listBoxBooks.Size = new System.Drawing.Size(240, 251);
+            this.listBoxBooks.TabIndex = 2;
+            this.listBoxBooks.SelectedIndexChanged += new System.EventHandler(this.listBoxBooks_SelectedIndexChanged);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(344, 60);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // txtGenre
+            // 
+            this.txtGenre.Location = new System.Drawing.Point(344, 97);
+            this.txtGenre.Name = "txtGenre";
+            this.txtGenre.Size = new System.Drawing.Size(100, 20);
+            this.txtGenre.TabIndex = 3;
+            // 
+            // txtLanguage
+            // 
+            this.txtLanguage.Location = new System.Drawing.Point(344, 140);
+            this.txtLanguage.Name = "txtLanguage";
+            this.txtLanguage.Size = new System.Drawing.Size(100, 20);
+            this.txtLanguage.TabIndex = 3;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(344, 181);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(100, 20);
+            this.txtYear.TabIndex = 3;
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(344, 218);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(100, 20);
+            this.txtISBN.TabIndex = 3;
+            // 
+            // txtFileUrl
+            // 
+            this.txtFileUrl.Location = new System.Drawing.Point(344, 261);
+            this.txtFileUrl.Name = "txtFileUrl";
+            this.txtFileUrl.Size = new System.Drawing.Size(100, 20);
+            this.txtFileUrl.TabIndex = 3;
+            // 
+            // txtPreviewUrl
+            // 
+            this.txtPreviewUrl.Location = new System.Drawing.Point(344, 302);
+            this.txtPreviewUrl.Name = "txtPreviewUrl";
+            this.txtPreviewUrl.Size = new System.Drawing.Size(100, 20);
+            this.txtPreviewUrl.TabIndex = 3;
+            // 
+            // txtCoverUrl
+            // 
+            this.txtCoverUrl.Location = new System.Drawing.Point(344, 339);
+            this.txtCoverUrl.Name = "txtCoverUrl";
+            this.txtCoverUrl.Size = new System.Drawing.Size(100, 20);
+            this.txtCoverUrl.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(32, 336);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(113, 336);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(197, 336);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
             // Form_Book
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtCoverUrl);
+            this.Controls.Add(this.txtPreviewUrl);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.txtFileUrl);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtLanguage);
+            this.Controls.Add(this.txtGenre);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.listBoxBooks);
             this.Controls.Add(this.pictureBox_exit);
             this.Controls.Add(this.label1);
             this.Name = "Form_Book";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form_Book";
+            this.Load += new System.EventHandler(this.Form_Book_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_exit)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -73,5 +193,17 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox_exit;
+        private System.Windows.Forms.ListBox listBoxBooks;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtGenre;
+        private System.Windows.Forms.TextBox txtLanguage;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.TextBox txtFileUrl;
+        private System.Windows.Forms.TextBox txtPreviewUrl;
+        private System.Windows.Forms.TextBox txtCoverUrl;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
