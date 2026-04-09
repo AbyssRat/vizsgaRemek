cd0f786ff3f6dbd21949e5c26a7489f979195cce
Molnár Dóra <yunothefurry@gmail.com>
2026-04-09 16:13:32 +0200

    res back

--- backend/routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getBooks, getBook } from "../controllers/bookController.js";
-
-const router = express.Router();
-
-router.get("/", getBooks);
-router.get("/:id", getBook);
-
-export default router;
